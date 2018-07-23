5f911863a7ae6c40b865385b3330859e47a6475f
yanis457824 <yanis457824@gmail.com>
2018-07-23 09:58:44 +0300

    Adding travel details

--- www/app/apps/docApprove/TRAVEL/travelDetailsCtrl.js
+++ www/app/apps/docApprove/TRAVEL/travelDetailsCtrl.js
@@ -33,7 +33,7 @@ angular.module('pele')
           var apiData = PelApi.checkApiResponse(data);
           if (apiData.error) return false;
           $scope.docDetails = PelApi.getJsonString(apiData.Result, "JSON[0]", true);
-          $scope.title = "טופס נסיעות לחול";
+          $scope.title = "נסיעות לחול";
           $scope.docDetails.attachments = $scope.docDetails.ATTACHMENT_FILES || [];
           PelApi.extendActionHistory($scope.docDetails);
           $scope.buttonsArr = $scope.docDetails.BUTTONS || [];
