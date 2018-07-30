80a977de1a0e106e52eabd520b563e3b1a42c2a5
golanh <golanh@localhost.localdomain>
2018-07-30 15:53:31 +0300

    wip

--- www/app/P1_appsListsCtrl.js
+++ www/app/P1_appsListsCtrl.js
@@ -6,7 +6,7 @@ var app = angular.module('pele.P1_appsListCtrl', ['ngStorage', 'ngCordova']);
 //==                         PAGE_1                                  ==//
 //=====================================================================//
 app.controller('P1_appsListCtrl',
-  function($scope, $http, $state, $ionicLoading, PelApi, ApiGateway, $rootScope, $ionicPopup, $ionicHistory, $sessionStorage, $localStorage, appSettings, srvShareData, $cordovaNetwork, $ionicNavBarDelegate) {
+  function($ionicScrollDelegate, $scope, $http, $state, $ionicLoading, PelApi, ApiGateway, $rootScope, $ionicPopup, $ionicHistory, $sessionStorage, $localStorage, appSettings, srvShareData, $cordovaNetwork, $ionicNavBarDelegate) {
     $ionicNavBarDelegate.showBackButton(true);
     $ionicHistory.clearHistory();
     PelApi.lagger.checkFile().then(function(logStat) {
@@ -16,6 +16,8 @@ app.controller('P1_appsListCtrl',
         })
       }
     })
+    $ionicScrollDelegate.$getByHandle('menu1-handle').scrollTo('300px');
+
     $rootScope.menuItems = [];
     $scope.options = {
       loop: true,
