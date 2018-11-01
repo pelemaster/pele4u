daeb4422c78bcb4d5f22cb66c878998b5f64eb54
Emil Feldman <emil_feldman@yahoo.com>
2018-11-01 07:28:36 +0200

    Package details css issues fix, package list sorting

--- www/app/apps/cc/packageListCtrl.js
+++ www/app/apps/cc/packageListCtrl.js
@@ -36,8 +36,7 @@ angular.module('pele')
             state: state.state
           }).success(function(pdata) {
             packages = _.filter(pdata.PackageList, (e) => (e) && (e.Package != null));
-            state.packages = packages;
-            //$scope.states.push({state: state, packages: packages, visible: false});
+            state.packages = _.sortBy(packages, o => o.Package);
             //console.log(JSON.stringify($scope.states));
           }).error(function(error, httpStatus, headers, config) {
             ApiGateway.throwError(httpStatus, "CC get Packages List", config);
