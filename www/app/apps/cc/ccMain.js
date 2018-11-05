bc27dea224a9987867f0adcffb24a36e123bc76a
Emil Feldman <emil_feldman@yahoo.com>
2018-11-05 14:50:21 +0200

    remove all arrow functions not supported by ios

--- www/app/apps/cc/ccMain.js
+++ www/app/apps/cc/ccMain.js
@@ -2,11 +2,9 @@ angular.module('pele')
   .controller('ccMainCtrl', ['ApiGateway', '$scope', '$state', 'PelApi',
   function(ApiGateway, $scope, $state, PelApi) {
       $scope.title = "CC - Main";
-      alert('In ccMain controller');
       PelApi.showLoading();
       ApiGateway.get("cc/getenv", {
       }).success(function(data) {
-        alert('ApiGateway returned');
         $scope.envList = data.EnvList;
         $scope.envList = _.filter($scope.envList, function(e) { return  e.Environment != null; });
       }).error(function(error, httpStatus, headers, config) {
