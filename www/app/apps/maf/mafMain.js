844a2fae3f348fa3bceb60d2d0a89c5e669d34c6
Emil Feldman <emil_feldman@yahoo.com>
2018-11-12 13:39:09 +0200

    add MAF app

--- www/app/apps/maf/mafMain.js
+++ www/app/apps/maf/mafMain.js
@@ -2,6 +2,11 @@ angular.module('pele')
   .controller('mafMainCtrl', ['ApiGateway', '$scope', 'PelApi', 
   function(ApiGateway, $scope, PelApi) {
       $scope.title = "MAF - Main";
+      
+      $scope.progsInRange = function(prog) {
+        return (prog.actual_processes > prog.to_up_processes)||(prog.actual_processes < prog.from_up_processes);
+      }
+
       PelApi.showLoading();
       ApiGateway.get("maf/backlog", {
       }).success(function(data) {
